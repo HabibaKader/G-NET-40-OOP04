31b4e4d041781254a2cca5417d75913cc1ea3538
HabibaKader <habibakader@outlook.com>
2026-03-02 01:29:54 +0200

    Part01: Question02

--- Assignment04_OOP/Program.cs
+++ Assignment04_OOP/Program.cs
@@ -16,6 +16,13 @@
             // used in overriding method
 
             #endregion
+
+            #region Part01: Question02
+
+            // method overloading is when you have multiple methods with the same name but different parameters in the same class
+            // method overriding is when you have a method in a child class that has the same name and parameters as a method in the parent class but acts different
+
+            #endregion
         }
     }
 }
