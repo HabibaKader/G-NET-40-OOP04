749418ff03fd2fcbf641be4bf18ecf5ad733e227
HabibaKader <habibakader@outlook.com>
2026-03-02 01:27:20 +0200

    Part01: Question01

--- Assignment04_OOP/Program.cs
+++ Assignment04_OOP/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part01: Question01
+
+            // What is the difference between static binding and dynamic binding?
+            //When does each one happen?
+
+            // Static binding happens at compile time
+            // used in overloading method
+
+            // Dynamic binding happens at runtime
+            // used in overriding method
+
+            #endregion
         }
     }
 }
